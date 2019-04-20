6e8617170b6ca645d6afc1e5d2d54abb470950f7
Iván Morett <luisivanmorett@gmail.com>
2019-04-20 09:48:50 -0500

    Another two solved problems

--- renderer/index.js
+++ renderer/index.js
@@ -5,11 +5,16 @@ const fileDialog = require('file-dialog')
 const FileReader = require('../helpers/FileReader')
 const Parentheses = require('../problems/Parentheses');
 const mergelist = require('../problems/mergelist');
+const trains = require('../problems/trains');
+const words = require('../problems/words');
 
 
 const radios = document.getElementsByName('problems');
 const btn = document.getElementById('fileBtn');
 const tableBody = document.getElementById("tableBody");
+const table = document.getElementById("table");
+const toast = document.getElementById("toast");
+const toastBtn = document.getElementById("toastBtn");
 
 document.getElementById('parentheses').value = problems.parentheses;
 document.getElementById('mergelist').value = problems.mergeList;
@@ -20,25 +25,55 @@ document.getElementById('travelSalesMan').value = problems.travelSalesMan;
 document.getElementById('dijkstra').value = problems.dijkstra;
 document.getElementById('prims').value = problems.prims;
 
+table.style.display ='none';
+toast.style.display ='none';
+
+toastBtn.onclick = ()=>{toast.style.display ='none';};
+
 btn.onclick = ()=>{
     cleanTable();
-    fileDialog({ multiple: false })
-    .then(file =>{
-        for(let i = 0; i < radios.length; i++){
-            if(radios[i].checked){
+    let checked = false;
+    for(let i = 0; i < radios.length; i++){
+        if(radios[i].checked){
+            toast.style.display ='none'; 
+            checked = true; 
+            fileDialog({ multiple: false })
+            .then(file =>{
                 const data = new FileReader(file[0].path)
-                solve(parseInt(radios[i].value), data)
-            }
-        }    
-    })
-    
+                solve(parseInt(radios[i].value), data) 
+            })   
+        }
+    }
+    if(!checked){
+        toast.style.display ='block';    
+    }
 }
 
 function solve(problemId, data){
     switch(problemId){ 
         case problems.parentheses:
-            parentheses(data);
-        break;
+            for(let i = 0; i<data.input.length; i++)
+            {
+                const output = new Parentheses(data.input[i]).results;
+                const desiredOutput = data.output[i].split(' ').map(value=>{
+                    return parseFloat(value);
+                })
+                let passed = true;
+                if(output.length == desiredOutput.length){
+                    for(let i = 0; i < output.length; i++){
+                        if(desiredOutput.indexOf(output[i])==-1)
+                        {
+                            passed = false;
+                            break;
+                        }
+                    }
+                }
+                else{
+                    passed = false;
+                }
+                addResultsRow(output, desiredOutput, passed);
+            }
+            break;
         case problems.mergeList:
             const res = mergelist(data);
             for(let i =0; i<res.length; i++){
@@ -46,42 +81,27 @@ function solve(problemId, data){
             }
         break;
         case problems.trains:
-            trains(data);
+            const trainsRes = trains(data);
+            for(let i =0; i<trainsRes.length; i++){
+                addResultsRow(trainsRes[i], parseInt(data.output[i]), trainsRes[i]===parseInt(data.output[i]));
+            }
+        break;
+        case problems.words:
+            const wordsResult = words(data);
+            for(let i =0; i<wordsResult.length; i++){
+                addResultsRow(wordsResult[i], data.output[i], wordsResult[i]==(data.output[i]=='true'));
+            }
         break;
     }
+    table.style.display ='block';
 }
-
-function parentheses(data){
-    for(let i = 0; i<data.input.length; i++)
-        {
-            const output = new Parentheses(data.input[i]).results;
-            const desiredOutput = data.output[i].split(' ').map(value=>{
-                return parseFloat(value);
-            })
-            let passed = true;
-            if(output.length == desiredOutput.length){
-                for(let i = 0; i < output.length; i++){
-                    if(desiredOutput.indexOf(output[i])==-1)
-                    {
-                        passed = false;
-                        break;
-                    }
-                }
-            }
-            else{
-                passed = false;
-            }
-            addResultsRow(output, desiredOutput, passed);
-        }
-}
-
-
 
 function addResultsRow(output, desired, passed){
-    const icon = passed?'<i class="icon icon-check"></i>':'<i class="icon icon-cross"></i>'
-    tableBody.innerHTML = tableBody.innerHTML + `<tr><td>${output}</td><td>${desired}</td><td>${passed}${icon}</td></tr>`
+    const icon = passed?'<i class="icon icon-check text-success"></i>':'<i class="icon icon-cross text-error"></i>'
+    tableBody.innerHTML = tableBody.innerHTML + `<tr><td>${output}</td><td>${desired}</td><td>${icon}</td></tr>`
 }
 
 function cleanTable(){
     tableBody.innerHTML = "";
+    table.style.display ='none';
 }
